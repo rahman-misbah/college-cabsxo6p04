6b87aa5b7e986b4db2bcba8878cc44ba1ea25ccc
Misbahur Rahman <rahman.misbah2604@gmail.com>
2026-03-13 10:17:40 +0530

    feat: Add Data type and its validator

--- modules/bit_block/_bit_block_types.py
+++ modules/bit_block/_bit_block_types.py
@@ -4,6 +4,7 @@ from typing import TypeIs, Union, Optional
 from bit_block import BitBlock
 
 type RawData = Union[int, bytes]
+type Data = Union[int, BitBlock]
 
 def is_valid_raw_data(data: RawData, block_size: int) -> TypeIs[RawData]:
     """Check if the input data is valid for the given block size.
@@ -20,6 +21,17 @@ def is_valid_raw_data(data: RawData, block_size: int) -> TypeIs[RawData]:
     if isinstance(data, bytes) and len(data) <= block_size // 8:
         return True
     return False
+
+def is_valid_data(data: Data) -> TypeIs[Data]:
+    """Check if the input data is valid (can be an integer or a BitBlock instance).
+    
+    Args:
+        data: The input data to check (can be an integer or a BitBlock instance).
+    
+    Returns:
+        True if the data is valid, False otherwise.
+    """
+    return isinstance(data, (int, BitBlock))
 
 def is_valid_block_size(block_size: int) -> TypeIs[int]:
     """Check if the block size is valid (must be a positive multiple of 8).
